66987ac536dc2fa9e94e9cfa043e2722d928bd9f
Eric Sal <eric.salavat@gmail.com>
2023-10-24 15:51:38 -0700

    Changed to using ChatOpenAI with messages and template for more flexibility.

--- main.py
+++ main.py
@@ -2,19 +2,33 @@ import os
 
 from dotenv import load_dotenv
 
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate
+)
 
 load_dotenv()
 
-OPEN_API_KEY = os.environ.get("OPEN_API_KEY")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", default="gpt-3.5-turbo")
+OPENAI_API_KEY = os.environ.get("OPEN_API_KEY")
+
+PROMPT_LANGUAGE_INFO = """
+    Provide a 2 to 3 sentence summary about the programming language {language}.
+"""
 
 def main():
-    llm = OpenAI(openai_api_key=OPEN_API_KEY)
-    result = llm.predict("give me 5 business ideas")
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model=OPENAI_MODEL)
+    
+    language = input("Enter the name of a programming language: ")
 
+    messages = []
+    messages.append(HumanMessagePromptTemplate.from_template(template=PROMPT_LANGUAGE_INFO))
+    chatPrompt = ChatPromptTemplate.from_messages(messages=messages)
+    chatPromptWithValues = chatPrompt.format_prompt(language=language)
+
+    result = llm(chatPromptWithValues.to_messages())
     print(result)
-
-    pass
 
 if __name__ == "__main__":
     main()
