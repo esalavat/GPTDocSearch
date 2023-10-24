c9e3c482a440bd2f01108b2de250a63a29c7aff5
Eric Sal <eric.salavat@gmail.com>
2023-10-24 16:16:39 -0700

    Added pydantic parser to format the results.

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import os
 from dotenv import load_dotenv
 
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
-    HumanMessagePromptTemplate,
-    ChatPromptTemplate
-)
+from langchain.prompts.chat import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+class Language(BaseModel):
+    released: str = Field(description="Year released.")
+    creator: str = Field(description="Name of creator.")
+    summary: str = Field(description="Short summary")
+
 
 load_dotenv()
 
@@ -15,20 +20,23 @@ OPENAI_API_KEY = os.environ.get("OPEN_API_KEY")
 
 PROMPT_LANGUAGE_INFO = """
     Provide a 2 to 3 sentence summary about the programming language {language}.
+    {format_instructions}
 """
 
 def main():
     llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model=OPENAI_MODEL)
-    
+    parser = PydanticOutputParser(pydantic_object=Language)
+
     language = input("Enter the name of a programming language: ")
 
     messages = []
     messages.append(HumanMessagePromptTemplate.from_template(template=PROMPT_LANGUAGE_INFO))
     chatPrompt = ChatPromptTemplate.from_messages(messages=messages)
-    chatPromptWithValues = chatPrompt.format_prompt(language=language)
+    chatPromptWithValues = chatPrompt.format_prompt(language=language, format_instructions=parser.get_format_instructions())
 
     result = llm(chatPromptWithValues.to_messages())
-    print(result)
+    parsedResult = parser.parse(result.content)
+    print(parsedResult)
 
 if __name__ == "__main__":
     main()
